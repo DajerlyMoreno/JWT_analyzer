19d6704b55109881facffc156ca355724c2033ea
danieldd57 <daniel.vargas19@uptc.edu.co>
2025-11-21 19:33:05 -0500

    server changes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,26 @@
-// server.js
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path"; 
+import { fileURLToPath } from 'url'; 
 import { connectDB } from "./config/db.js";
 import jwtRoutes from "./routes/jwt.routes.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '..', 'frontend'))); 
+
 // Conexión a MongoDB
 connectDB();
 
-// Rutas
-app.use("/api", jwtRoutes);
+// Rutas de la API (solo bajo el prefijo /api)
+app.use("/api", jwtRoutes); 
 
 const PORT = process.env.PORT || 3000;
 
@@ -25,3 +31,5 @@ export default app;
 if (process.env.JEST_WORKER_ID === undefined) {
   app.listen(PORT, () => console.log(`✅ Servidor en http://localhost:${PORT}`));
 }
+
+
